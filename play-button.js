f7bffc452f90ac57e111a127e1c405c108fdccb6
kalong228 <48005055+kalong228@users.noreply.github.com>
2019-05-19 18:33:09 +0800

    .

--- play-button.js
+++ play-button.js
@@ -2,9 +2,27 @@
 // it toggles audio playback.
 //
 // See HW4 writeup for more hints and details.
+
 class PlayButton {
   constructor() {
-    // TODO(you): Implement the constructor and add fields as necessary.
+    this.click = this.click.bind(this);
+    this.playing = true;
+    this.btn = document.getElementById("play");
+    this.btn = addEventListener('click',this.click);
+    this.count = 0;
   }
-  // TODO(you): Add methods as necessary.
+  click(event){
+   // console.log("count = "+this.count);
+    this.count++;
+    if(this.count%2==0){
+      document.getElementById("play").style.backgroundImage = "url(images/pause.png)";
+      this.playing = true;
+      app.music.audio.play();
+    }
+    else{
+      document.getElementById("play").style.backgroundImage = "url(images/play.png)";
+      this.playing = false;
+      app.music.audio.pause();
+    }
+  }
 }
